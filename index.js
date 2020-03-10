5f24c5450bef6a66eeb5f6898a14b299c6fc9258
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-10 20:35:57 +0530

    createWindow()

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ const url = require('url')
 require('electron-context-menu')()
 
 let mainWindow
+
+function createWindow(){
+  mainWindow = new BrowserWindow({
+    backgroundColor: '#000',
+    backgroundColor: '#000',
+    darkTheme: true,
+    width: 1000,
+    height: 400,
+    minWidth: 800,
+    minHeight: 200,
+    acceptFirstMouse: true
+  })
+
+  mainWindow.loadURL(url.format({
+  pathname: path.join(__dirname, 'app', 'index.html'),
+  protocol: 'file:',
+  slashes: true
+  }))
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
