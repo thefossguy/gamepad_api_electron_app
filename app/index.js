b79168230e3e033cf5f7b95011e9de33ef7f13fb
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-10 20:46:43 +0530

    updated app/index.js

--- app/index.js
+++ app/index.js
@@ -5,3 +5,26 @@ var app = nano()
 app.view(view)
 app.use(store)
 app.mount('#app')
+
+function view (state, emit) {
+  var gamepads = Object.values(state.controllers)
+  var title = x('p.title')
+
+  return x('#app')(
+    gamepads.length < 1
+      ? 'Press a button on your controller to start'
+      : gamepads.map((gamepad, idx) =>
+        x('.gamepad')(
+          title(`${idx + 1}: ${gamepad.id}`),
+          title('Buttons'),
+          x('.buttons')(
+            buttons(gamepad.buttons)
+          ),
+          title('Axes'),
+          x('.axes')(
+            axes(gamepad.axes)
+          )
+        )
+      )
+  )
+}
