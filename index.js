3e39f90e9bd7196fb8d1522983e51ae31f544df5
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-10 20:38:03 +0530

    updated index.js

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ function createWindow(){
     mainWindow = null
   })
 }
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
